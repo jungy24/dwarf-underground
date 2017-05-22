1911f079fdf21bce5dc024c21d77b3db4803dd79
Your Full Name <jungy24@gmail.com>
2017-05-22 14:21:43 -0400

    Added Article component

---
+++ src/Article.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import './Article.css'
+
+class Article extends Component {
+  render() {
+    return (
+        <div class="large-8 medium-12 columns article">
+        <h2 class="article-title">Gold Madness - Fact or Fiction?</h2>
+        <div class="avatar">
+          <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
+          <div class="author-info">
+            <p class="author-name">By Domri, son of Flug</p>
+            <p class="date">on 28 April, 3018 of the Third Age</p>
+          </div>
+        </div>
+        <section class="article-body">
+          <p><span class="article-start gold">E</span>psum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
+
+          <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
+
+          <p>Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Gratuitous octopus niacin, sodium glutimate.</p>
+
+          <p class="interject">Avast! A stray goblin!</p>
+
+          <p>Quote meon an estimate et non interruptus stadium. Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito.</p>
+
+          <p>Epsum factorial non deposit quid pro quo hic escorol. Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum. Defacto lingo est igpay atinlay. Marquee selectus non provisio incongruous feline nolo contendre. Gratuitous octopus niacin, sodium glutimate. Quote meon an estimate et non interruptus stadium.</p>
+
+          <p>Sic tempus fugit esperanto hiccup estrogen. Glorious baklava ex librus hup hey ad infinitum. Non sequitur condominium facile et geranium incognito. Epsum factorial non deposit quid pro quo hic escorol. Marquee selectus non provisio incongruous feline nolo contendre Olypian quarrels et gorilla congolium sic ad nauseum. Souvlaki ignitus carborundum e pluribus unum.</p>
+        </section>
+        <div class="article-links">
+          <a class="article-link" href="#">
+            <i class="fa fa-comments-o"></i>
+            <span class="article-link-text">Comments</span>
+          </a>
+          <a class="article-link" href="#">
+            <i class="fa fa-share"></i>
+            <span class="article-link-text">Share Post</span>
+          </a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Article
